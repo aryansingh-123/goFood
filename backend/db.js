366367578869a94d0938321bed47c5f9dbdb0197
aryansingh-123 <aksingh2558@gmail.com>
2024-05-04 21:18:57 +0530

    Trying to make connection

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const mongoURI = 'mongodb+srv://ayran582002:Aryan@123@gofood.r9etesr.mongodb.net/?retryWrites=true&w=majority&appName=goFood'
 const connectDB =async() =>{
-    
+    mongoose.set('strictQuery', true);
     mongoose.connect(mongoURI,{ useNewUrlParser: true },(err, result)=>{
         if(err) console.log("Error in conneting with mongoDb",err)
         else{
@@ -9,8 +9,4 @@ const connectDB =async() =>{
     }
     });
 }
-
 module.exports = connectDB;
-
-
-
