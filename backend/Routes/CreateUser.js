673a2f4e13fa1c1f8f6c6972b5635fbd3f97da17
aryansingh-123 <aksingh2558@gmail.com>
2024-05-10 18:45:14 +0530

    added signup and login

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
+const { body, validationResult } = require('express-validator');
+
+router.post("/createuser",[
+body('email').isEmail(),
+body('name').isLength({min : 5}),
+body('password','incorrect Password').isLength({min : 5})], async (req, res)=>{
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+
+       return res.status(400).json({ errors: errors.array() });
+    }
 
 
-router.post("/createuser", async (req, res)=>{
+
+
+
+
     try {
         await User.create({
             name:req.body.name,
--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,20 @@ const port = 5000
 const connectDB = require('./db');
 connectDB();
 
+app.use((req, res, next)=>{
+    res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    )
+    next();
+
+})
+
 app.get('/', (req, res) =>{
     res.send('Hey Welcome to backend')
 })
-app.use(express.json());
+app.use(express.json() );
 app.use('/api', require("./Routes/CreateUser"));
 app.listen(port, () =>{
     console.log(`app is listening on port ${port}`)
